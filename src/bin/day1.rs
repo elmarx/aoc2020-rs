ae9f03beb29167471234ff66e42b83d7954016e3
Elmar Athmer <elmar@athmer.org>
2020-12-01 16:41:50 +0100

    solve day 1, part 1

---
+++ src/bin/day1.rs
@@ -0,0 +1,36 @@
+const INPUT: &str = include_str!("../../input/day1_1.txt");
+
+fn main() {
+    let input: Vec<i32> = INPUT.split("\n").map(|l| l.parse().unwrap()).collect();
+
+    let (a, b) = find_2020_tuple(&input).expect("did not find any tuple summing up to 2020 :'(");
+    let answer = a * b;
+
+    println!("Answer: {0}. ({1} * {2} = {0})", answer, a, b);
+}
+
+/// from the list of input, find the two numbers that add up to 2020
+fn find_2020_tuple(input: &[i32]) -> Option<(i32, i32)> {
+    // simply walk through all numbers…
+    input.iter().enumerate().find_map(|(index, &a)| {
+        input
+            .iter()
+            // if we checked a + b, no need to check b + a, so skip all previous values
+            .skip(index)
+            .find(|&b| a + b == 2020)
+            // if an actual number has been found, return a and b as tuple
+            .map(|&b| (a, b))
+    })
+}
+
+#[cfg(test)]
+mod test {
+    use crate::find_2020_tuple;
+
+    #[test]
+    fn test_sample() {
+        let sample = &[1721, 979, 366, 299, 675, 1456];
+        let actual = find_2020_tuple(sample);
+        assert_eq!(actual, Some((1721, 299)));
+    }
+}
