61ff9c638e164dbf0fbd101a69426c4cac637340
Elmar Athmer <elmar@athmer.org>
2020-12-01 16:43:49 +0100

    introduce rayon (because I can)

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,3 +1,5 @@
+use rayon::prelude::*;
+
 const INPUT: &str = include_str!("../../input/day1_1.txt");
 
 fn main() {
@@ -12,12 +14,12 @@ fn main() {
 /// from the list of input, find the two numbers that add up to 2020
 fn find_2020_tuple(input: &[i32]) -> Option<(i32, i32)> {
     // simply walk through all numbers…
-    input.iter().enumerate().find_map(|(index, &a)| {
+    input.par_iter().enumerate().find_map_any(|(index, &a)| {
         input
-            .iter()
+            .par_iter()
             // if we checked a + b, no need to check b + a, so skip all previous values
             .skip(index)
-            .find(|&b| a + b == 2020)
+            .find_any(|&b| a + b == 2020)
             // if an actual number has been found, return a and b as tuple
             .map(|&b| (a, b))
     })
