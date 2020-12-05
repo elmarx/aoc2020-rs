725c804df9af0d0c7400f04213c46ec7dd338dce
Elmar Athmer <elmar@athmer.org>
2020-12-05 11:21:16 +0100

    solve day 5, part 2

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 const INPUT: &str = include_str!("../../input/day5.txt");
 
 fn get_seat_id(s: &str) -> u16 {
@@ -8,13 +10,37 @@ fn get_seat_id(s: &str) -> u16 {
         .fold(0_u16, |acc, bit| (acc << 1) + bit)
 }
 
-fn main() {
-    let answer1 = INPUT.split('\n').map(get_seat_id).max().unwrap();
+fn find_my_seat(boarding_pass_ids: &[u16]) -> u16 {
+    boarding_pass_ids
+        .iter()
+        // boarding passes are unordered, so bring them in order before checking for our seat
+        .sorted()
+        // look at our potential neighbors
+        .tuple_windows::<(&u16, &u16)>()
+        .find_map(|(a, b)| {
+            // if there is exactly one seat (ID) between, that's our seat!
+            let my_seat_a = *a + 1;
+            let my_seat_b = *b - 1;
+            if my_seat_a == my_seat_b {
+                Some(my_seat_a)
+            } else {
+                None
+            }
+        })
+        .unwrap()
+}
 
+fn main() {
+    let boarding_pass_ids = INPUT.split('\n').map(get_seat_id).collect_vec();
+
+    let answer1 = *boarding_pass_ids.iter().max().unwrap();
     println!(
         "Answer 1: {} is the highest seat ID on a boarding pass",
         answer1
-    )
+    );
+
+    let answer2 = find_my_seat(&boarding_pass_ids);
+    println!("Answer 2: {} is my seat ID", answer2);
 }
 
 #[cfg(test)]
