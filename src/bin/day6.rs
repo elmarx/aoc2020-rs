6b7b559af2659fabbcb8a0c18d11e56513d8aee7
Elmar Athmer <elmar@athmer.org>
2020-12-06 11:53:20 +0100

    solve day 6, part 2

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::collections::HashSet;
 
 const INPUT: &str = include_str!("../../input/day6.txt");
@@ -14,14 +15,31 @@ fn count_groups(i: &str) -> Vec<usize> {
         })
         .collect()
 }
+
+fn count_common_answers(i: &str) -> Vec<usize> {
+    i.split("\n\n")
+        .map(|group| {
+            group
+                .lines()
+                .map(|passenger| passenger.bytes().collect::<HashSet<u8>>())
+                .fold1(|a, b| a.intersection(&b).map(|e| *e).collect())
+                .unwrap()
+                .len()
+        })
+        .collect()
+}
+
 fn main() {
     let answer1 = count_groups(INPUT).iter().sum::<usize>();
     println!("Answer 1: {}", answer1);
+
+    let answer2 = count_common_answers(INPUT).iter().sum::<usize>();
+    println!("Answer 2: {}", answer2);
 }
 
 #[cfg(test)]
 mod test {
-    use crate::count_groups;
+    use crate::{count_common_answers, count_groups};
 
     const SAMPLE: &str = include_str!("../../input/day6_sample.txt");
 
@@ -31,5 +49,13 @@ mod test {
         let expected = vec![3, 3, 3, 1, 1];
 
         assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn test_count_common_answers() {
+        let actual = count_common_answers(SAMPLE);
+        let expected = vec![3, 0, 1, 1, 1];
+
+        assert_eq!(actual, expected);
     }
 }
